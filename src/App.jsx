8a325fc91961800abbe3f5acfb047d5953d19cde
LuSilvaDeveloper <lusilva.dev@icloud.com>
2025-04-23 22:38:12 -0700

    added Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-function App() {
-  return <main>
-    <div id="game-container">
-      <ol id="players">
-        <li>
-          <span className="player-name">Player 1</span>
-          <span className="player-symbol">X</span>
-        </li>
-        <li>
-          <span className="player-name">Player 2</span>
-          <span className="player-symbol">O</span>
-        </li>
-      </ol>
-      Game Board
-    </div>
+import Player from "./components/Player";
 
-    Log
-  </main>;
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        Game Board
+      </div>
+      Log
+    </main>
+  );
 }
 
 export default App;
